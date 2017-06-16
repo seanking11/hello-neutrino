cab9f02a2d9afb1adfe6a7ea09024d4696d9878d
Sean King <kingsc@miamioh.edu>
2017-06-16 13:25:44 -0500

    Initial commit

---
+++ src/Welcome/styled.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const Welcome = styled.h1`
+  color: red;
+`
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Welcome from './Welcome/index.js'
+
+render(<Welcome name="Sean" />, document.getElementById('root'));
